dcb682bb81daff652b252e098145c6cb969b9275
bhanupgpt <98256435+bhanupgpt@users.noreply.github.com>
2023-07-02 19:27:03 +0000

    added html and css to index.html

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route("/") #to match to home page
 
 def hello_world():
-  return "helloshfgdf"
+  return render_template('index.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
