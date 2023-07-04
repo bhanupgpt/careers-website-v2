e8d1205e04b3ef2cd875f1ba56340838c80c90e8
bhanupgpt <98256435+bhanupgpt@users.noreply.github.com>
2023-07-04 08:38:12 +0000

    added connection to mysql database

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Data Analyst',
-    'location': 'Bengaluru, India',
-    'salary': 'Rs. 12,00,000'
-  },
-  {
-    'id': 2,
-    'title': 'Front End Engineer',
-    'location': 'Delhi, India',
-    'salary': 'Rs. 10,00,000'
-  },
-  {
-    'id': 3,
-    'title': 'Data Scientist',
-    'location': 'Remote',
-    'salary': 'Rs. 15,00,000'
-  },
-  {
-    'id': 4,
-    'title': 'Backend Engineer',
-    'location': 'San Francisco, USA',
-    'salary': '120,000 USD '
-  },
-]
+# JOBS = [
+#   {
+#     'id': 1,
+#     'title': 'Data Analyst',
+#     'location': 'Bengaluru, India',
+#     'salary': 'Rs. 12,00,000'
+#   },
+#   {
+#     'id': 2,
+#     'title': 'Front End Engineer',
+#     'location': 'Delhi, India',
+#     'salary': 'Rs. 10,00,000'
+#   },
+#   {
+#     'id': 3,
+#     'title': 'Data Scientist',
+#     'location': 'Remote',
+#     'salary': 'Rs. 15,00,000'
+#   },
+#   {
+#     'id': 4,
+#     'title': 'Backend Engineer',
+#     'location': 'San Francisco, USA',
+#     'salary': '120,000 USD '
+#   },
+# ]
+
 
 # to create html route
 @app.route("/")  #to match to home page
 def hello_world():
-  return render_template('index.html', jobs=JOBS)
- 
+  jobs_list = load_jobs_from_db()
+  return render_template('index.html', jobs=jobs_list)
+
+
 # to create api route
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs_list = load_jobs_from_db()
+  return jsonify(jobs_list)
 
 
 if __name__ == "__main__":
